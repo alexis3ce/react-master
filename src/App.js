c2ed47a8edce76658cf6364f1129559bc946fff4
DESKTOP-KGAFJHU\Miremo <mondanoalexis@gmail.com>
2023-10-08 23:47:10 +0800

    add props, state"

--- src/App.js
+++ src/App.js
@@ -1,41 +1,63 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Card from './Card';
-import { faker } from '@faker-js/faker';
+import styled, { ThemeProvider, css } from 'styled-components'
+import axios from 'axios'
+
+const theme = {
+  primary: '#4CAF50',
+  mango: 'yellow'
+}
+const Button = styled.button`
+  border: none;
+  ${props =>
+    props.color && css`
+  background-color: ${props => props.length > 2 ? '#4CAF50' : props.length < 2 ? 'red' : 'pink'};
+  color: ${props => props.length <= 1 ? 'black' : 'white'};
+  `
+  }
+  font-weight:${props => props.length <= 1 ? 'bold' : 'normal'};
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+`
 
 function App() {
-  const [name, setName] = useState('Allan Smith');
-  const [showCard, setShowCard] = useState(true)
-  const toggleShowCard = () => setShowCard(!showCard)
-  const cardMarkup = showCard && 
-  <Card 
-    avatar='https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/809.jpg'
-    name={name}
-    title='Chief Intranet Manager'
-  />
+  const [card, setCard] = useState([]);
+  const [id, setId] = useState(1)
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+    .then(res => {
+      console.log(res.data)
+      setCard(res.data)
+    })
+  }, [id])
 
-  // avatar='https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/477.jpg'
-  // name='Miranda Langworth'
-  // title='Product Accountability Executive'
-
-  // avatar='https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/758.jpg'
-  // name='Phillip Schoen I'
-  // title='Investor Group Coordinator'
-
-  // avatar='https://avatars.githubusercontent.com/u/22411976'
-  // name='Randy O'Hara Sr.'
-  // title='Forward Brand Coordinator'
+  const changeNameHandler = (event, id)=> {
+    // 1. make a copy of the cards
+    const card_copy = {...card}
+    // 2. change the name of the specific card
+    card_copy.name = event.target.value
+    // 3. set the cards with the latest version of card copy
+    setCard(card_copy)
+  }
 
   return (
-    <div className="App">
-      <button className="button" onClick={toggleShowCard}>Toggle</button>
-      {cardMarkup}
-      <Card
-        avatar={faker.image.avatar()}
-        name={faker.person.fullName()}
-        title={faker.person.jobTitle()}
-      />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <input type='text' value={id} onChange={e=> setId(e.target.value)}/>
+        <Card 
+          name={card.name}
+          phone={card.phone}
+          key={card.id}
+          onChangeName={(event) => changeNameHandler(event, card.id)}
+        />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -2,14 +2,13 @@ import React from 'react'
 import './App.css';
 
 const Card = props => {
-    console.log(props)
   return (
     <div className="card">
-        <img src={props.avatar} alt="Avatar" style={{width: '100%'}}/>
         <div className="container">
             <h4><b>{props.name}</b></h4> 
-            <p>{props.title}</p> 
-            <p><button className='button button-red' onClick={props.onChangeName}>Delete</button></p>
+            <p>{props.phone}</p> 
+            <input type='text' onChange={props.onChangeName} value={props.name}/>
+            <p><button className='button button-red' onClick={props.onDelete}>Delete</button></p>
         </div>
     </div>
   )
